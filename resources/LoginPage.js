0d116ea294e77f0b51b6e6f535fcf747c76999a3
Sathish kumar Santhanagopalan <skumar5@netspend.com>
2019-02-07 21:18:45 -0600

    Login page

--- resources/LoginPage.js
+++ resources/LoginPage.js
@@ -14,10 +14,10 @@ export default class BootScreen extends Component {
 			<View style = {styles.container}>
 			
 				<TextInput placeholder = 'Phone Number'
-					placeholderTextColor= 'black'
+					placeholderTextColor= 'grey'
 					style = {styles.phoneNumText}
 				/>
-				
+				<Text /> 
 				<TouchableHighlight style={styles.buttonLoginContainer}>
 						<Text style={styles.buttonLoginText}> LOGIN </Text>
 					</TouchableHighlight>
@@ -33,19 +33,27 @@ const styles = StyleSheet.create({
 		backgroundColor: '#FFF'
 	},
 	phoneNumText: {
-		height: 40
-	}
+		marginTop: '50%',
+		marginLeft: '12.5%',
+		marginRight: '12.5%',
+		height: 40,
+		textAlign: 'left',
+		paddingLeft: 10,
+		fontSize: 20,
+		borderColor: 'gray',
+		borderWidth: 1,
+		borderRadius: 5,
+		width: '75%',
+	},
 
 	buttonLoginContainer: {
 		backgroundColor: '#3b5998',
 		padding: 10,
 		alignItems: 'center',
 		width:'75%',
+		borderRadius: 5,
 		marginLeft: '12.5%',
-		marginRight: '12.5%',
-		marginBottom: '2%',
-		borderRadius: 5
-
+		
 	},
 
 	buttonLoginText: {
